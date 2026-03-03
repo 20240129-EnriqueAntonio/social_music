9b03445f6c42e25a50b2185367c3f477a2af0f7d
20240129-EnriqueAntonio <20240129@utsh.edu.mx>
2026-03-02 18:14:48 -0600

    INIT solo login y registro

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Login from './Login'
+import Registro from './registro'
+
+
+function App() {
+  return (
+    <div className="App">
+      {/*<Login />*/}
+      <Registro />
+      
+    </div>
+  )
+
+  
+}
+
+export default App
